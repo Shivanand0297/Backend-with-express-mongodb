d9da5f9bb212f8972b1c7f989a708cdfaceca661
Shivanand <Shivanand0297@gmail.com>
2022-11-28 13:33:51 +0530

    initial commit

---
+++ 06 crud app/frontend/src/App.js
@@ -0,0 +1,13 @@
+import Form from './components/Form';
+import UserLists from './components/UserLists';
+
+function App() {
+  return (
+    <div className="App">
+      <Form/>
+      <UserLists/>
+    </div>
+  );
+}
+
+export default App;
