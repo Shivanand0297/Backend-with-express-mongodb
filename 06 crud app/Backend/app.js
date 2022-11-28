d15066ab334d06e1bdc478d8bcb3fa4e1c26ebc7
Shivanand <Shivanand0297@gmail.com>
2022-11-28 16:46:09 +0530

    installed cors and cors options

--- 06 crud app/Backend/app.js
+++ 06 crud app/Backend/app.js
@@ -1,12 +1,19 @@
 require("dotenv").config()
 const express = require("express")
+const cors = require("cors")
 require("./config/dbConnect").connectToDb()
 const userRoutes = require("./routes/userRoutes")
 
+const corsOptions ={
+    origin:'http://127.0.0.1:3000', 
+    credentials:true,            //access-control-allow-credentials:true
+    optionSuccessStatus:200
+}
+
 const app = express()
+app.use(cors(corsOptions))
 app.use(express.json())
 app.use(express.urlencoded({extended: true}))
-
 app.use("/", userRoutes)
 
 module.exports = app
